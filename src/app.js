c9b950223c1ead3ca5f37d029f8b16b185a7eb91
soumen <soumenmaity106@gmail.com>
2018-08-21 18:03:03 +0530

    Node js Crude project

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+//connection to db
+mongoose.connect("mongodb://localhost/crud-mongo")
+.then(db =>console.log('Db Connected'))
+.catch(err => console.log(err));
+
+
+//importing routes
+const indexRoutes = require('./routes/index'); 
+//Setting
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares 
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+
+// route
+
+app.use('/',indexRoutes);
+
+//staring the server  
+app.listen(app.get('port'), () =>{
+    console.log(`Server on  port ${app.get('port')}`)
+})
